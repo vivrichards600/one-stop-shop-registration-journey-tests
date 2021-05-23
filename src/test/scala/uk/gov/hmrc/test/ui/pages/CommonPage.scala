1da13fdfdfaf7eefd39f0329c65dfb2fd2a2e4a4
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2021-05-23 11:30:16 +0100

    Revert change to selectAnswer method

--- src/test/scala/uk/gov/hmrc/test/ui/pages/CommonPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/CommonPage.scala
@@ -58,7 +58,7 @@ object CommonPage extends BrowserDriver with Matchers {
     data match {
       case "yes" => driver.findElement(By.id("value")).click()
       case "no"  => driver.findElement(By.id("value-no")).click()
-      case id    => driver.findElement(By.id(id)).click()
+      case _     => throw new Exception("Option doesn't exist")
     }
     driver.findElement(By.xpath("//*[@id='main-content']/div/div/form/button")).click()
   }
